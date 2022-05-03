95e85441889745ac622e73c1f45462a3b98ab9b0
Deivide <dejs_silva@hotmail.com>
2022-05-03 08:39:37 -0300

    Testando metodos Get e GetBYId

--- Back/src/ProEvento.API/Controllers/EventoController.cs
+++ Back/src/ProEvento.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEvento.API.Models;
 
 namespace ProEvento.API.Controllers
 {
@@ -11,15 +12,48 @@ namespace ProEvento.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
+        public IEnumerable<Evento> _evento = new Evento[] {
+                new Evento(){
+                EventoId = 1,
+                Tema = "Angular 11 e .Net 5",
+                Local = "Belo Orizonte",
+                Lote = "1 Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/mm/yyyy"),
+                ImagemURL = "foto.png"
+
+            },
+
+                new Evento(){
+                EventoId = 2,
+                Tema = "Angular e suias novidades",
+                Local = "São Paulo",
+                Lote = "2 Lote",
+                QtdPessoas = 350,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/mm/yyyy"),
+                ImagemURL = "foto1.png"
+
+            }
+
+            };
         public EventoController()
         {
-           
+
         }
 
         [HttpGet]
-        public string Get()
+        public IEnumerable<Evento> Get()
         {
-            return "Exemplo de Get";
+
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+
+            return _evento.Where(evento => evento.EventoId == id);
+           
         }
 
         [HttpPost]
@@ -27,6 +61,11 @@ namespace ProEvento.API.Controllers
         {
             return "Exemplo de Post";
         }
+
+    
+
+
+
         [HttpPut("{id}")]
         public string Put(int id)
         {
@@ -37,6 +76,7 @@ namespace ProEvento.API.Controllers
         {
             return $"Exemplo de Delete = {id}";
         }
+
 
 
     }
